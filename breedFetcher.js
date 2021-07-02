59a620ae0c4648781ae461834b14d09843de4701
Hadjira <bkh.hadjira@gmail.com>
2021-07-02 02:59:44 +0000

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,24 @@
 const request = require('request');
-var params = process.argv
-            .slice(2);
-var parameter =  params[0];
-var url = "https://api.thecatapi.com/v1/breeds/search"+'?q=' + parameter;             
-request(url,(error,response,body) =>{
-  if (error){
-   console.log(error);
-  }else{
-    const data = JSON.parse(body);
-    if ( data.length == 0){
-      console.log('the breed %s is not found.',parameter);
-    }else {
-      console.log(data[0].description);
+
+var url = "https://api.thecatapi.com/v1/breeds/search";             
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  url = url + '?q=' + breedName;
+  request(url,(error,response,body) =>{
+    if (error){
+     callback(error,null);
+    }else{
+      const data = JSON.parse(body);
+      if ( data.length == 0){
+        var errorMessage ='the breed '+ breedName + ' is not found.';
+        callback(errorMessage, null);
+      }else {
+        callback(null,data[0].description);
+      }
     }
-  }
- 
-})
+   
+  })
+};
+
+module.exports = { fetchBreedDescription };
